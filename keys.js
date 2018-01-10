4b6ed13879849f3406334f9663f4c14fd5eddb07
Brandon Haines <haines.brandon.91@gmail.com>
2018-01-10 10:03:40 -0700

    keys made

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: '5PSDKejgJ1cFvLPkB3SBsqo4s',
+  consumer_secret: 'bLzeCALrGRE9W0jJtDNnSGy4jSsWIgUhim1Vrazy0yG53yKLtC',
+  access_token_key: '369576879-9EBvD2ibNQ1ZETxQCLuea6KRB8QeAQzFCd1aTdhr',
+  access_token_secret: 'hUkrDUup75exOKNFbjfWKVaDyfjdzW07QnMcd6PYbtGMH',
+}
+
+module.exports = twitterKeys;
---
+++ liri.js
@@ -0,0 +1,1 @@
+var dataKeys = require('./keys.js');
