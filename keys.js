26c516ce0c6cb95fb9abd7e68abf04a34254827a
Brandon Haines <haines.brandon.91@gmail.com>
2018-01-17 08:25:11 -0700

    made spotify and do something work

--- keys.js
+++ keys.js
@@ -4,14 +4,15 @@ var twitterKeys = {
   consumer_key: '5PSDKejgJ1cFvLPkB3SBsqo4s',
   consumer_secret: 'bLzeCALrGRE9W0jJtDNnSGy4jSsWIgUhim1Vrazy0yG53yKLtC',
   access_token_key: '369576879-9EBvD2ibNQ1ZETxQCLuea6KRB8QeAQzFCd1aTdhr',
-  access_token_secret: 'hUkrDUup75exOKNFbjfWKVaDyfjdzW07QnMcd6PYbtGMH',
+  access_token_secret: 'hUkrDUup75exOKNFbjfWKVaDyfjdzW07QnMcd6PYbtGMH'
 }
-
-module.exports = twitterKeys;
 
 var spotifyKeys = {
-  consumer_key: '542a65241d124a959fecacb83a92ad70',
-  consumer_secret: '250639c7ae774a8bb0a989991c894c91',
+  id: '542a65241d124a959fecacb83a92ad70',
+  secret: '250639c7ae774a8bb0a989991c894c91'
 }
 
-module.exports = spotifyKeys;
+module.exports = {
+  twitterKeys: twitterKeys,
+  spotifyKeys: spotifyKeys
+}
--- liri.js
+++ liri.js
@@ -1,11 +1,12 @@
 var dataKeys = require('./keys.js');
-console.log(dataKeys);
+
 var Twitter = require('twitter');
 var Spotify = require('node-spotify-api');
 var request = require('request');
+var fs = require('fs');
 
 var client = new Twitter(dataKeys.twitterKeys);
-var getSpotify = new Spotify(dataKeys.spotifyKeys);
+var spotify = new Spotify(dataKeys.spotifyKeys);
 
 // console.log(client)
 
@@ -23,58 +24,92 @@ console.log(process.argv[2])
 var userRequest = process.argv[2]
 
 
-var movieName = "";
+var userInput = "";
 
 // Loop through all the words in the node argument
 // And do a little for-loop magic to handle the inclusion of "+"s
 for (var i = 3; i < process.argv.length; i++) {
 
-  if (movieName == "") {
+  if (userInput == "") {
 
-    movieName = movieName + process.argv[i];
+    userInput = userInput + process.argv[i];
 
   }
 
   else {
 
-    movieName = movieName +  " " +  process.argv[i];
+    userInput = userInput +  " " +  process.argv[i];
 
   }
 }
-console.log(movieName);
+console.log(userInput);
 // if(userRequest === "my-tweets"){
 //   //do this stuff
 // }else if(){
 
 // }
 
-switch (userRequest) {
-  case "my-tweets":
-    //do this stuff
-    getTweets()
-    break;
-  case "spotify-this-song":
-    //do this stuff
-    getSpotify()
-    break;
-  case "do-what-it-says":
-    //do this stuff
-    getSpotify()
-    break;
-  case "movie-this":
-    //do this stuff
-    getMovies()
-    break;
+function commands(fileCommand, fileSearch){
+  // console.log(arguments)
+  // console.log(arguments['0'], arguments['1'])
+  if (arguments["0"] == undefined, arguments["1"] == undefined){
 
-  default:
-    break;
+    switch (userRequest) {
+      case "my-tweets":
+        //do this stuff
+        getTweets()
+        break;
+      case "spotify-this-song":
+        //do this stuff
+        getSpotify()
+        break;
+      case "do-what-it-says":
+        //do this stuff
+        doWhat()
+        break;
+      case "movie-this":
+        //do this stuff
+        getMovies()
+        break;
+
+      default:
+        break;
+    }
+
+  } else {
+
+    switch (fileCommand) {
+      case "my-tweets":
+        //do this stuff
+        getTweets()
+        break;
+      case "spotify-this-song":
+        //do this stuff
+        getSpotify(fileSearch)
+        break;
+      case "do-what-it-says":
+        //do this stuff
+        doWhat()
+        break;
+      case "movie-this":
+        //do this stuff
+        getMovies()
+        break;
+    
+      default:
+        break;
+    }
+  }
 }
-
+commands()
 
 
 function getTweets() {
   //This will show your last 20 tweets and when they were created at in your terminal / bash window.
-  var params = { screen_name: "NFL" };
+  if(userInput == ""){
+    userInput = "NFL"
+  }
+  var params = { screen_name: userInput };
   
   client.get('statuses/user_timeline', params, function (error, tweets, response) {
     if (!error) {
@@ -91,26 +126,63 @@ function getTweets() {
   });  
 }
 
-function getSpotify() {
-    
-    getSpotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+function getSpotify(fileSearch) {
+  if (arguments['0'] === undefined){
+    if(userInput == ""){
+      userInput = "The Sign Ace of Base"
+    }
+    spotify.search({ type: 'track', query: userInput }, function(err, data) {
     if (err) {
         return console.log('Error occurred: ' + err);
-    for (var i = 0; i < songs.length; i++) { 
+      }
+      var songs = data.tracks.items;
+      // console.log(songs)
+      // console.log("=====================================")
+      // console.log("Arist Name: " + songs.artists[0].name);
+      // console.log("Song: " + songs.name);
+      // console.log("URL: " + songs.external_urls.spotify);
+      // console.log("Album: " + songs.album.name);
+  
+  
+      for (var i = 0; i < songs.length; i++) { 
         console.log("=====================================")
-        console.log(songs[i].artists);
-        console.log(songs[i].name);
-        console.log(songs[i].preview_url);
-        console.log(songs[i].album.name);
-        }  
-    }
-    console.log(data); 
+        console.log("Arist Name: " + songs[i].artists[0].name);
+        console.log("Song: " + songs[i].name);
+        console.log("URL: " + songs[i].external_urls.spotify);
+        console.log("Album: " + songs[i].album.name);
+          }  
+  
     });
+
+  }else{
+    spotify.search({ type: 'track', query: fileSearch }, function (err, data) {
+      if (err) {
+        return console.log('Error occurred: ' + err);
+      }
+      var songs = data.tracks.items;
+      // console.log(songs)
+      // console.log("=====================================")
+      // console.log("Arist Name: " + songs.artists[0].name);
+      // console.log("Song: " + songs.name);
+      // console.log("URL: " + songs.external_urls.spotify);
+      // console.log("Album: " + songs.album.name);
+
+
+      for (var i = 0; i < songs.length; i++) {
+        console.log("=====================================")
+        console.log("Arist Name: " + songs[i].artists[0].name);
+        console.log("Song: " + songs[i].name);
+        console.log("URL: " + songs[i].external_urls.spotify);
+        console.log("Album: " + songs[i].album.name);
+      }
+
+    });
+  }
 }
 
 function getMovies(){
 
-  request('http://www.omdbapi.com/?t=' + movieName + '&y=&plot=short&apikey=trilogy', function (error, response, body) {
+  request('http://www.omdbapi.com/?t=' + userInput + '&y=&plot=short&apikey=trilogy', function (error, response, body) {
     console.log('error:', error); // Print the error if one occurred
     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
     console.log('body:', body); // Print the HTML for the Google homepage.
@@ -123,8 +195,23 @@ function getMovies(){
     console.log('Language: ' , JSON.parse(body).Language);
     console.log('Plot: ' , JSON.parse(body).Plot);
     console.log('Actors: ' , JSON.parse(body).Actors);
-    console.log('Rotten Tomatoes Rating: ', JSON.parse(body).tomatoRating);
-    console.log('Rotton Tomatoes URL: ', JSON.parse(body).tomatoURL);  
+    console.log('Rotten Tomatoes Rating: ', JSON.parse(body).Ratings[1].Value);
+    console.log('Website: ', JSON.parse(body).Website);  
   
     }
+    
   )};
+
+  function doWhat (){
+    fs.readFile('./random.txt', "utf8", (err, data) => {
+      if (err) throw err;
+      // console.log(data.split(","));
+      var file = data.split(",")
+
+      var fileCommand = file[0]
+      var fileSearch = file[1]
+      // console.log(object);
+      userRequest = null
+    commands(fileCommand, fileSearch)
+    });
+  }
